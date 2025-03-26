f59704f7c9d0740fd6fcb90c709ad32d429b1612
onzoro-rento <rento0725ken1009@icloud.com>
2025-03-26 12:42:25 +0900

    second commit

---
+++ backend/resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {reactive} from 'vue';
+import {Inertia} from '@inertiajs/inertia';
+
+const form = reactive({
+    title: null,
+    content: null
+})
+
+const submitFunction = () => {
+    Inertia.post('/inertia',form)
+}
+</script>
+
+<template>
+    <form @submit.prevent = "submitFunction">
+        <input name = "title" v-model ="form.title" type="text"><br>
+        <input name = "content" v-model = "form.content" type="text">
+        <button>送信</button>
+    </form>
+</template>
---
+++ backend/resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+ああああ
+</template>
